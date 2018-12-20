edc973879163f4e4f5628272067c2890bd6515ec
Malelion <gert3987@student.eadania.dk>
2018-12-20 13:32:52 +0100

    Create refl.php

---
+++ refl.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="da">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portefolio</title>
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i|Source+Sans+Pro:400,400i,600,600i" rel="stylesheet">
+    <link rel="stylesheet" href="css/main-style.css" type="text/css" media="all">
+    <link rel="stylesheet" href="css/menu-style.css">
+</head>
+    <body>
+        <?php 
+            include "reflmenu.html"; 
+            include "include/refl.html";
+            include "include/footer.inc";    
+        ?>
+    </body>
+<html>
